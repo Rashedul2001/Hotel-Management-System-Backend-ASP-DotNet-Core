7f505fab030515f94a42130c9dfbd4f353879cf1
MD Rashedul Hasan <rashedul2001cse@gmail.com>
2026-09-11 00:13:38 +0600

    Add login logout registration

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using Hotel_Management_System_Backend_dotNet.DTOs.Auth;
+﻿using System.Security.Claims;
+using Hotel_Management_System_Backend_dotNet.DTOs.Auth;
 using Hotel_Management_System_Backend_dotNet.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +10,21 @@ namespace Hotel_Management_System_Backend_dotNet.Controllers
 {
     [Route("api/auth")]
     [ApiController]
-    public class AuthController(UserManager<ApplicationUser> userManager) : ControllerBase
+    public class AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager = signInManager;
+
+
+        // ---------------------------------------------------------
+        // REGISTER
+        // it will try to register the user and if successful, it will automatically log the user in.
+        // ---------------------------------------------------------
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterRequestDto request)
         {
-            if (request is null || string.IsNullOrWhiteSpace(request.FullName) || string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Password) || string.IsNullOrWhiteSpace(request.ConfirmPassword) )
+            if (request is null || string.IsNullOrWhiteSpace(request.FullName) || string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Password) || string.IsNullOrWhiteSpace(request.ConfirmPassword))
             {
                 return BadRequest(new
                 {
@@ -28,7 +37,18 @@ namespace Hotel_Management_System_Backend_dotNet.Controllers
                 {
                     message = "Password and Confirm Password do not match."
                 });
+
             }
+
+            if (!request.AcceptTerms)
+            {
+                return BadRequest(new
+                {
+                    message = "You must accept the terms and conditions to register."
+                });
+            }
+
+
             var existingUser = await _userManager.FindByEmailAsync(request.Email);
             if (existingUser is not null)
             {
@@ -37,6 +57,7 @@ namespace Hotel_Management_System_Backend_dotNet.Controllers
                     message = "An Account with this Email already exists."
                 });
             }
+
 
             var user = new ApplicationUser
             {
@@ -53,12 +74,174 @@ namespace Hotel_Management_System_Backend_dotNet.Controllers
                 return BadRequest(new
                 {
                     message = "Registration failed.",
-                    errors = result.Errors
+                    errors = result.Errors.Select(error => new
+                    {
+                        code = error.Code,
+                        description = error.Description
+                    })
                 });
             }
 
-            return Ok(new { Message = "Registration Successful" });
+            // Automatically log the newly registered user in.
+            var signInResult = await _signInManager.PasswordSignInAsync(
+                user,
+                request.Password,
+                isPersistent: false,
+                lockoutOnFailure: false
+            );
 
+            if (!signInResult.Succeeded)
+            {
+                return StatusCode(500, new
+                {
+                    message = "Account was created, but automatic login failed."
+                });
+            }
+
+            return Ok(new
+            {
+                message = "Registration successful.",
+                user = new
+                {
+                    id = user.Id,
+                    fullName = user.FullName,
+                    userName = user.UserName,
+                    email = user.Email
+                }
+            });
+
+        }
+
+        // ---------------------------------------------------------
+        // LOGIN
+        // ---------------------------------------------------------
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDto request)
+        {
+            if (
+                request is null ||
+                string.IsNullOrWhiteSpace(request.EmailOrUserName) ||
+                string.IsNullOrWhiteSpace(request.Password)
+            )
+            {
+                return BadRequest(new
+                {
+                    message = "Username/email and password are required."
+                });
+            }
+
+            ApplicationUser? user = null;
+
+            if (request.EmailOrUserName.Contains('@'))
+            {
+                user = await _userManager.FindByEmailAsync(
+                    request.EmailOrUserName
+                );
+            }
+
+            user ??= await _userManager.FindByNameAsync(
+                request.EmailOrUserName
+            );
+
+            if (user is null)
+            {
+                return Unauthorized(new
+                {
+                    message = "Invalid username/email "
+                });
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                user,
+                request.Password,
+                request.RememberMe,
+                //for security reasons, we will lock the user out after 5 failed attempts for 5 minutes
+                lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                return Ok(new
+                {
+                    message = "Login successful."
+                });
+            }
+
+            if (result.IsLockedOut)
+            {
+                return Unauthorized(new
+                {
+                    message = "Your account is temporarily locked. Please try again later."
+                });
+            }
+
+            if (result.IsNotAllowed)
+            {
+                return Unauthorized(new
+                {
+                    message = "You are not allowed to log in."
+                });
+            }
+
+            return Unauthorized(new
+            {
+                message = "Invalid username/email or password."
+            });
+        }
+
+        //---------------------------------------------------------
+        // Who am I 
+        // ---------------------------------------------------------
+        [Authorize]
+        [HttpGet("me")]
+        public async Task<IActionResult> Me()
+        {
+            var userId =
+                User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userId is null)
+            {
+                return Unauthorized();
+            }
+
+            var user =
+                await _userManager.FindByIdAsync(userId);
+
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            // TODO: Roles shall be implemented in the future. For now, we will not return roles in the response.
+
+            // var roles =
+            //     await _userManager.GetRolesAsync(user);
+
+            return Ok(new
+            {
+                id = user.Id,
+                fullName = user.FullName,
+                userName = user.UserName,
+                email = user.Email,
+                // roles
+            });
+        }
+
+
+        // ---------------------------------------------------------
+        // LOGOUT
+        // ---------------------------------------------------------
+
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Ok(new
+            {
+                message = "Logout successful."
+            });
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -20,7 +20,8 @@ builder.Services
     .AddIdentityApiEndpoints<ApplicationUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-var frontendBaseUrl = builder.Configuration["Frontend:BaseUrl"]?? "http://localhost:3000"; // Default to localhost if not set in configuration
+var frontendBaseUrl = builder.Configuration["Frontend:BaseUrl"] ??
+ throw new InvalidOperationException("Frontend base URL is not configured in appsettings.json or environment variables.");
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("NextJsFrontend", policy =>
@@ -34,16 +35,12 @@ builder.Services.AddCors(options =>
 });
 
 
-builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
-
-builder.Services.AddControllers();
 
 /* Configure custom error response globally 
  *without this the ASP .NET Core will return a default error response for validation errors, 
  *which may not be user-friendly or consistent with your API's error handling strategy 
  */
-
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.InvalidModelStateResponseFactory = context =>
@@ -53,7 +50,7 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
         IEnumerable<object> errors = [];
         if (env.IsDevelopment())
         {
-            
+
             errors = context.ModelState
                 .Where(e => e.Value != null && e.Value.Errors.Count > 0)
                 .Select(e => new { Field = e.Key, Error = e.Value?.Errors.First()?.ErrorMessage ?? "Unknown error" });
@@ -79,9 +76,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
 
-/*below 3 lines should be in this order to work properly */
+/*below 4 lines should be in this order to work properly */
+app.UseHttpsRedirection();
 app.UseCors("NextJsFrontend");
 app.UseAuthentication();
 app.UseAuthorization();
